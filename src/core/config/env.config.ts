31db114280b9d11a35343ae19f403b0d943e45f0
Mohan Rathore <dev.smartmohan20@gmail.com>
2026-01-30 03:12:43 +0530

    Implemented centralized config file for env variables

---
+++ src/core/config/env.config.ts
@@ -0,0 +1,48 @@
+import { plainToInstance } from 'class-transformer';
+import { IsEnum, IsNumber, IsString, validateSync } from 'class-validator';
+
+export enum Environment {
+    Development = 'development',
+    Test = 'test',
+    Production = 'production',
+}
+
+export class EnvironmentVariables {
+    @IsEnum(Environment)
+    NODE_ENV: Environment;
+
+    @IsNumber()
+    PORT: number;
+
+    @IsString()
+    DATABASE_URL: string;
+
+    @IsString()
+    JWT_SECRET: string;
+
+    @IsString()
+    GOOGLE_CLIENT_ID: string;
+
+    @IsString()
+    GOOGLE_CLIENT_SECRET: string;
+
+    @IsString()
+    GOOGLE_CALLBACK_URL: string;
+
+}
+
+export function validate(config: Record<string, unknown>) {
+    const validatedConfig = plainToInstance(
+        EnvironmentVariables,
+        config,
+        { enableImplicitConversion: true },
+    );
+    const errors = validateSync(validatedConfig, {
+        skipMissingProperties: false,
+    });
+
+    if (errors.length > 0) {
+        throw new Error(errors.toString());
+    }
+    return validatedConfig;
+}
