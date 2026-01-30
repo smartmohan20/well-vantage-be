46b7ee3230f223eddcbcda0c2e90f2b0c0c8f0d8
Mohan Rathore <dev.smartmohan20@gmail.com>
2026-01-30 11:09:15 +0530

    Implemented app module

---
+++ src/app.module.ts
@@ -0,0 +1,38 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggerModule } from './shared/logger/logger.module';
+import { PrismaModule } from './shared/prisma/prisma.module';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { AllExceptionsFilter } from './core/filters/all-exceptions.filter';
+import { TransformInterceptor } from './core/interceptors/transform.interceptor';
+import { validate } from './core/config/env.config';
+
+/**
+ * Root module of the application.
+ * Responsibility: Aggregate all modules, configure global filters and interceptors.
+ */
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            validate,
+        }),
+        LoggerModule,
+        PrismaModule,
+        UsersModule,
+        AuthModule,
+    ],
+    providers: [
+        {
+            provide: APP_FILTER,
+            useClass: AllExceptionsFilter,
+        },
+        {
+            provide: APP_INTERCEPTOR,
+            useClass: TransformInterceptor,
+        },
+    ],
+})
+export class AppModule { }
