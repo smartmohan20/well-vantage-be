17f911d9f8afec570f45773f6637edb177f21355
Mohan Rathore <dev.smartmohan20@gmail.com>
2026-01-30 11:08:50 +0530

    Implemented transform interpreter

---
+++ src/core/interceptors/transform.interceptor.ts
@@ -0,0 +1,33 @@
+import {
+    Injectable,
+    NestInterceptor,
+    ExecutionContext,
+    CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface Response<T> {
+    data: T;
+}
+
+/**
+ * Global interceptor to transform response data.
+ * Wraps the response data in a standard 'data' object.
+ */
+@Injectable()
+export class TransformInterceptor<T>
+    implements NestInterceptor<T, Response<T>> {
+    /**
+     * Intercepts and transforms the response.
+     * @param context - Execution context.
+     * @param next - Call handler to continue the request.
+     * @returns Observable that emits the transformed response.
+     */
+    intercept(
+        context: ExecutionContext,
+        next: CallHandler,
+    ): Observable<Response<T>> {
+        return next.handle().pipe(map((data) => ({ data })));
+    }
+}
