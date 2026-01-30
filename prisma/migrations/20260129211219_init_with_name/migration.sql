8e3aab510e2b9259eb8e11afc3f992e8841c23f0
Mohan Rathore <dev.smartmohan20@gmail.com>
2026-01-30 11:34:02 +0530

    Changes on migration file

--- prisma/migrations/20260129211219_init_with_name/migration.sql
+++ prisma/migrations/20260129211219_init_with_name/migration.sql
@@ -4,6 +4,7 @@ CREATE TABLE "User" (
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "name" TEXT NOT NULL,
+    "refreshToken" TEXT,
     "googleId" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
